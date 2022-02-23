af3fde050676d3a746546964de3e2f37ccd1f31c
yomizo <morning.going.0420@gmail.com>
2022-02-24 07:57:40 +0900

    add iframe for executing field

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,7 @@ import { unpkgPathPlugin } from "./plugins/unpkg-path-plugin";
 
 const App = () => {
   const ref = useRef<any>();
+  const iframe = useRef<any>();
   const [input, setInput] = useState("");
   const [code, setCode] = useState("");
 
@@ -36,10 +37,22 @@ const App = () => {
       },
     });
 
-    // console.log(result);
-
-    setCode(result.outputFiles[0].text);
+    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, "*");
   };
+
+  const html = `
+    <html>
+      <head></head>
+      <body>
+        <div id="root"></div>
+        <script>
+          window.addEventListener('message', (event) => {
+            eval(event.data);
+          }, false)
+        </script>
+      </body>
+    </html>
+  `;
 
   return (
     <div>
@@ -51,6 +64,12 @@ const App = () => {
         <button onClick={onClick}>Submit</button>
       </div>
       <pre>{code}</pre>
+      <iframe
+        ref={iframe}
+        sandbox="allow-scripts"
+        srcDoc={html}
+        title="iframe"
+      />
     </div>
   );
 };
