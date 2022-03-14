8299be8188eb4edf113f885d055a66c7838402cd
yomizo <morning.going.0420@gmail.com>
2022-03-15 07:11:50 +0900

    add redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,18 @@
 import "bulmaswatch/superhero/bulmaswatch.min.css";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 import CodeCell from "./components/code-sell";
 import TextEditor from "./components/text-editor";
+import { store } from "./state";
 
 const App = () => {
   return (
-    <div>
-      <TextEditor />
-      <CodeCell />
-    </div>
+    <Provider store={store}>
+      <div>
+        <TextEditor />
+        <CodeCell />
+      </div>
+    </Provider>
   );
 };
 
